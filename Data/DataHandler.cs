8e00e152a4b3f278be4e206530e293b78a3963dd
Vukasin1Radalj <sproutx1@gmail.com>
2025-12-22 16:59:13 +0100

    Dopunjen DataHandler sa novim podacima, greske ispeglane, dopunjen main

--- Data/DataHandler.cs
+++ Data/DataHandler.cs
@@ -10,20 +10,96 @@ namespace SajamKnjiga.Data
 {
     internal class DataHandler
     {
-     
-        private static string putanjaPosetioci = "podaci" + Path.DirectorySeparatorChar + "posetioci.txt";
+        private static string folder = "podaci";
+        private static string putanjaPosetioci = Path.Combine(folder, "posetioci.txt");
+        private static string putanjaIzdavaci = Path.Combine(folder, "izdavaci.txt");
+        private static string putanjaKupovine = Path.Combine(folder, "kupovine.txt");
+        private static string putanjaKnjige = Path.Combine(folder, "knjige.txt");
+        private static string putanjaAutori = Path.Combine(folder, "autori.txt");
 
+        private static void ProveriFolder()
+        {
+            if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
+        }
+
+        // --- AUTORI ---
+        public static void SacuvajAutore(List<Autor> autori)
+        {
+            ProveriFolder();
+            using (StreamWriter sw = new StreamWriter(putanjaAutori))
+            {
+                foreach (var a in autori)
+                {
+                    sw.WriteLine($"{a.Ime}|{a.Prezime}|{a.BrojLicneKarte}|{a.Email}|{a.GodineIskustva}|{a.Telefon}");
+                }
+            }
+        }
+
+        public static List<Autor> UcitajAutore()
+        {
+            List<Autor> rezultat = new List<Autor>();
+            if (!File.Exists(putanjaAutori)) return rezultat;
+
+            foreach (string linija in File.ReadAllLines(putanjaAutori))
+            {
+                string[] d = linija.Split('|');
+                if (d.Length < 6) continue;
+                rezultat.Add(new Autor
+                {
+                    Ime = d[0],
+                    Prezime = d[1],
+                    BrojLicneKarte = d[2],
+                    Email = d[3],
+                    GodineIskustva = int.Parse(d[4]),
+                    Telefon = d[5]
+                });
+            }
+            return rezultat;
+        }
+
+        // --- KNJIGE ---
+        public static void SacuvajKnjige(List<Knjiga> knjige)
+        {
+            ProveriFolder();
+            using (StreamWriter sw = new StreamWriter(putanjaKnjige))
+            {
+                foreach (var k in knjige)
+                {
+                    sw.WriteLine($"{k.ISBN}|{k.Naziv}|{k.Zanr}|{k.Cena}|{k.GodinaIzdanja}|{k.Izdavac}");
+                }
+            }
+        }
+
+        public static List<Knjiga> UcitajKnjige()
+        {
+            List<Knjiga> rezultat = new List<Knjiga>();
+            if (!File.Exists(putanjaKnjige)) return rezultat;
+
+            foreach (string linija in File.ReadAllLines(putanjaKnjige))
+            {
+                string[] d = linija.Split('|');
+                if (d.Length < 6) continue;
+                rezultat.Add(new Knjiga
+                {
+                    ISBN = d[0],
+                    Naziv = d[1],
+                    Zanr = d[2],
+                    Cena = double.Parse(d[3]),
+                    GodinaIzdanja = int.Parse(d[4]),
+                    Izdavac = d[5]
+                });
+            }
+            return rezultat;
+        }
+
+        // --- POSETIOCI ---
         public static void SacuvajPosetioce(List<Posetilac> posetioci)
         {
-          
-            string folder = "podaci";
-            if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
-
+            ProveriFolder();
             using (StreamWriter sw = new StreamWriter(putanjaPosetioci))
             {
                 foreach (var p in posetioci)
                 {
-                   
                     sw.WriteLine($"{p.Ime}|{p.Prezime}|{p.BrojClanskeKarte}|{p.Status}|{p.Email}");
                 }
             }
@@ -32,37 +108,31 @@ namespace SajamKnjiga.Data
         public static List<Posetilac> UcitajPosetioce()
         {
             List<Posetilac> rezultat = new List<Posetilac>();
-
             if (!File.Exists(putanjaPosetioci)) return rezultat;
 
-            using (StreamReader sr = new StreamReader(putanjaPosetioci))
+            foreach (string linija in File.ReadAllLines(putanjaPosetioci))
             {
-                string linija;
-                while ((linija = sr.ReadLine()) != null)
+                string[] d = linija.Split('|');
+                rezultat.Add(new Posetilac
                 {
-                    string[] delovi = linija.Split('|');
-                    Posetilac p = new Posetilac
-                    {
-                        Ime = delovi[0],
-                        Prezime = delovi[1],
-                        BrojClanskeKarte = delovi[2],
-                        Status = (StatusPosetioca)Enum.Parse(typeof(StatusPosetioca), delovi[3]),
-                        Email = delovi[4]
-                    };
-                    rezultat.Add(p);
-                }
+                    Ime = d[0],
+                    Prezime = d[1],
+                    BrojClanskeKarte = d[2],
+                    Status = (StatusPosetioca)Enum.Parse(typeof(StatusPosetioca), d[3]),
+                    Email = d[4]
+                });
             }
             return rezultat;
         }
-        private static string putanjaIzdavaci = "podaci" + Path.DirectorySeparatorChar + "izdavaci.txt";
 
+        // --- IZDAVAČI ---
         public static void SacuvajIzdavace(List<Izdavac> izdavaci)
         {
+            ProveriFolder();
             using (StreamWriter sw = new StreamWriter(putanjaIzdavaci))
             {
                 foreach (var i in izdavaci)
                 {
-
                     sw.WriteLine($"{i.Sifra}|{i.Naziv}|{i.SefIzdavaca?.Ime}|{i.SefIzdavaca?.Prezime}|{i.SefIzdavaca?.GodineIskustva}");
                 }
             }
@@ -73,37 +143,17 @@ namespace SajamKnjiga.Data
             List<Izdavac> rezultat = new List<Izdavac>();
             if (!File.Exists(putanjaIzdavaci)) return rezultat;
 
-            using (StreamReader sr = new StreamReader(putanjaIzdavaci))
+            foreach (string linija in File.ReadAllLines(putanjaIzdavaci))
             {
-                string linija;
-                while ((linija = sr.ReadLine()) != null)
+                string[] d = linija.Split('|');
+                rezultat.Add(new Izdavac
                 {
-                    string[] delovi = linija.Split('|');
-                    Izdavac i = new Izdavac
-                    {
-                        Sifra = delovi[0],
-                        Naziv = delovi[1],
-                        SefIzdavaca = new Autor { Ime = delovi[2], Prezime = delovi[3], GodineIskustva = int.Parse(delovi[4]) }
-                    };
-                    rezultat.Add(i);
-                }
+                    Sifra = d[0],
+                    Naziv = d[1],
+                    SefIzdavaca = new Autor { Ime = d[2], Prezime = d[3], GodineIskustva = int.Parse(d[4]) }
+                });
             }
             return rezultat;
         }
-
-        private static string putanjaKupovine = "podaci" + Path.DirectorySeparatorChar + "kupovine.txt";
-
-        public static void SacuvajKupovine(List<Kupovina> kupovine)
-        {
-            using (StreamWriter sw = new StreamWriter(putanjaKupovine))
-            {
-                foreach (var k in kupovine)
-                {
-                    sw.WriteLine($"{k.Posetilac?.Ime}|{k.Knjiga?.Naziv}|{k.DatumKupovine}|{k.Ocena}");
-                }
-            }
-        }
-
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,8 +3,6 @@ using SajamKnjiga.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SajamKnjiga
 {
@@ -12,17 +10,21 @@ namespace SajamKnjiga
     {
         static void Main(string[] args)
         {
+            // 1. Učitavanje svih podataka na startu
             List<Posetilac> sviPosetioci = DataHandler.UcitajPosetioce();
             List<Izdavac> sviIzdavaci = DataHandler.UcitajIzdavace();
+            List<Autor> sviAutori = DataHandler.UcitajAutore();
+            List<Knjiga> sveKnjige = DataHandler.UcitajKnjige();
 
             bool kraj = false;
             while (!kraj)
             {
-                Console.WriteLine("\n===== SAJAM KNJIGA - MENI =====");
-                Console.WriteLine("1. Prikaz svih posetilaca");
-                Console.WriteLine("2. Unos novog posetioca");
-                Console.WriteLine("3. Unos novog izdavaca (PROVERA ŠEFA)");
-                Console.WriteLine("4. Sačuvaj i izađi");
+                Console.WriteLine("\n===== SAJAM KNJIGA - GLAVNI MENI =====");
+                Console.WriteLine("1. Upravljanje Posetiocima");
+                Console.WriteLine("2. Upravljanje Izdavačima");
+                Console.WriteLine("3. Upravljanje Autorima");
+                Console.WriteLine("4. Upravljanje Knjigama");
+                Console.WriteLine("5. Sačuvaj i izađi");
                 Console.Write("Izbor: ");
 
                 string izbor = Console.ReadLine();
@@ -30,60 +32,109 @@ namespace SajamKnjiga
                 switch (izbor)
                 {
                     case "1":
-                        Console.WriteLine("\n--- Lista posetilaca ---");
-                        sviPosetioci.ForEach(p => Console.WriteLine(p));
+                        MeniPosetioci(sviPosetioci);
                         break;
-
                     case "2":
-                        Console.Write("Ime: "); string ime = Console.ReadLine();
-                        Console.Write("Prezime: "); string prezime = Console.ReadLine();
-                        Console.Write("Email: "); string email = Console.ReadLine();
-                        Console.Write("Broj članske karte: "); string bck = Console.ReadLine(); 
-                        Console.Write("Telefon: "); string tel = Console.ReadLine();
-
-                        sviPosetioci.Add(new Posetilac
-                        {
-                            Ime = ime,
-                            Prezime = prezime,
-                            Email = email,
-                            BrojClanskeKarte = bck,
-                            Telefon = tel,
-                            Status = StatusPosetioca.R, 
-                            GodinaClanstva = DateTime.Now.Year 
-                        });
-                        Console.WriteLine("Posetilac dodat u listu.");
+                        MeniIzdavaci(sviIzdavaci);
                         break;
-
                     case "3":
-                        Console.Write("Šifra izdavača: "); string sifraIzd = Console.ReadLine();
-                        Console.Write("Naziv izdavača: "); string nazivIzd = Console.ReadLine();
-                        Console.Write("Ime šefa: "); string imeSefa = Console.ReadLine();
-                        Console.Write("Prezime šefa: "); string prezimeSefa = Console.ReadLine(); 
-                        Console.Write("Godine iskustva šefa: ");
-                        int iskustvo = int.Parse(Console.ReadLine());
-
-                        if (iskustvo >= 5)
-                        {
-                            Autor sef = new Autor { Ime = imeSefa, Prezime = prezimeSefa, GodineIskustva = iskustvo };
-                            sviIzdavaci.Add(new Izdavac { Sifra = sifraIzd, Naziv = nazivIzd, SefIzdavaca = sef });
-                            Console.WriteLine("Izdavač uspešno dodat!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("GREŠKA: Šef mora imati najmanje 5 godina iskustva!");
-                        }
+                        MeniAutori(sviAutori);
                         break;
-
                     case "4":
+                        MeniKnjige(sveKnjige);
+                        break;
+                    case "5":
+                        // 2. Čuvanje apsolutno svega pre gašenja
                         DataHandler.SacuvajPosetioce(sviPosetioci);
                         DataHandler.SacuvajIzdavace(sviIzdavaci);
-                        Console.WriteLine("Podaci sačuvani. Pozdrav!");
+                        DataHandler.SacuvajAutore(sviAutori);
+                        DataHandler.SacuvajKnjige(sveKnjige);
+                        Console.WriteLine("Svi podaci su uspešno sačuvani u folder 'podaci'. Pozdrav!");
                         kraj = true;
                         break;
-
                     default:
                         Console.WriteLine("Nepostojeća opcija.");
                         break;
+                }
+            }
+        }
+
+        // --- POMOĆNE METODE ZA MENIJE ---
+
+        static void MeniPosetioci(List<Posetilac> posetioci)
+        {
+            Console.WriteLine("\n--- Posetioci ---");
+            Console.WriteLine("1. Prikaz svih");
+            Console.WriteLine("2. Unos novog");
+            string izb = Console.ReadLine();
+            if (izb == "1") posetioci.ForEach(p => Console.WriteLine(p));
+            else if (izb == "2")
+            {
+                Console.Write("Ime: "); string i = Console.ReadLine();
+                Console.Write("Prezime: "); string p = Console.ReadLine();
+                Console.Write("Broj članske: "); string b = Console.ReadLine();
+                posetioci.Add(new Posetilac { Ime = i, Prezime = p, BrojClanskeKarte = b, Status = StatusPosetioca.R });
+                Console.WriteLine("Posetilac dodat.");
+            }
+        }
+
+        static void MeniAutori(List<Autor> autori)
+        {
+            Console.WriteLine("\n--- Autori ---");
+            Console.WriteLine("1. Prikaz svih");
+            Console.WriteLine("2. Unos novog");
+            string izb = Console.ReadLine();
+            if (izb == "1") autori.ForEach(a => Console.WriteLine(a));
+            else if (izb == "2")
+            {
+                Console.Write("Ime: "); string i = Console.ReadLine();
+                Console.Write("Prezime: "); string p = Console.ReadLine();
+                Console.Write("LK: "); string lk = Console.ReadLine();
+                Console.Write("Iskustvo (god): "); int god = int.Parse(Console.ReadLine());
+                autori.Add(new Autor { Ime = i, Prezime = p, BrojLicneKarte = lk, GodineIskustva = god });
+                Console.WriteLine("Autor dodat.");
+            }
+        }
+
+        static void MeniKnjige(List<Knjiga> knjige)
+        {
+            Console.WriteLine("\n--- Knjige ---");
+            Console.WriteLine("1. Prikaz svih");
+            Console.WriteLine("2. Unos nove");
+            string izb = Console.ReadLine();
+            if (izb == "1") knjige.ForEach(k => Console.WriteLine(k));
+            else if (izb == "2")
+            {
+                Console.Write("Naziv: "); string n = Console.ReadLine();
+                Console.Write("ISBN: "); string isbn = Console.ReadLine();
+                Console.Write("Cena: "); double c = double.Parse(Console.ReadLine());
+                knjige.Add(new Knjiga { Naziv = n, ISBN = isbn, Cena = c });
+                Console.WriteLine("Knjiga dodata.");
+            }
+        }
+
+        static void MeniIzdavaci(List<Izdavac> izdavaci)
+        {
+            Console.WriteLine("\n--- Izdavači ---");
+            Console.WriteLine("1. Prikaz svih");
+            Console.WriteLine("2. Unos novog (min 5 god iskustva šefa)");
+            string izb = Console.ReadLine();
+            if (izb == "1") izdavaci.ForEach(i => Console.WriteLine(i));
+            else if (izb == "2")
+            {
+                Console.Write("Naziv: "); string naz = Console.ReadLine();
+                Console.Write("Šifra: "); string sif = Console.ReadLine();
+                Console.Write("Ime šefa: "); string im = Console.ReadLine();
+                Console.Write("Iskustvo šefa: "); int g = int.Parse(Console.ReadLine());
+
+                if (g >= 5)
+                {
+                    izdavaci.Add(new Izdavac { Naziv = naz, Sifra = sif, SefIzdavaca = new Autor { Ime = im, GodineIskustva = g } });
+                    Console.WriteLine("Izdavač dodat.");
+                }
+                else
+                {
+                    Console.WriteLine("Greška: Šef nema dovoljno iskustva.");
                 }
             }
         }
